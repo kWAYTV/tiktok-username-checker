cf5f3f63c1f1351951d9a65b1c8703c896bf981d
kWAY <55437151+kWAYTV@users.noreply.github.com>
2022-08-16 13:30:54 +0200

    Update b.py

--- b.py
+++ b.py
@@ -5,6 +5,10 @@ from pystyle import Colors, Colorate, Center
 clear = lambda: os.system("cls" if os.name in ("nt", "dos") else "clear") # Don't touch this
 users = open('check.txt', 'r').read().split('\n')
 count = 0
+free = 0
+taken = 0
+ratelimited = 0
+error = 0
 proxyDebug = False
 os.system(f"title Tiktok Username Checker - Starting...")
 clear()
@@ -22,7 +26,7 @@ def printLogo():
         print(Center.XCenter(Colorate.Horizontal(Colors.white_to_green, logo, 1)))
 
 def check():
-    global count
+    global count, free, taken, ratelimited, error
     session = requests.Session()
     while True:
         for line in users:
@@ -32,28 +36,29 @@ def check():
             else:
                 pass
             r = session.get(f'https://www.tiktok.com/@{line}', headers={'Connection': 'keep-alive', 'User-Agent': 'TikTok 25.7.4 rv:174014 (iPhone; iOS 16.0; sv_SE) Cronet'}, timeout=60)
+            count += 1
             if r.status_code == 200 or 204:
                 print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}] {Fore.RESET}Taken: " + line)
                 with open ("taken.txt", "a") as f:
                     f.write(line + "\n")
-                    count += 1
+                    taken += 1
             elif r.status_code == 404:
                 print(f"{Fore.RED}[{Fore.RESET}-{Fore.RED}] {Fore.RESET}Free: "+ line)
                 with open ("free.txt", "a") as f:
                     f.write(line + "\n")
-                    count += 1
+                    free += 1
             elif r.status_code == 429:
                 print(f"{Fore.YELLOW}[{Fore.RESET}!{Fore.YELLOW}] {Fore.RESET}Ratelimited: " + line + ". Sleeping for 30 seconds...")
                 with open ("ratelimited.txt", "a") as f:
                     f.write(line + "\n")
-                    count += 1
+                    ratelimited += 1
                 time.sleep(30)
             else:
                 print(f"{Fore.YELLOW}[{Fore.RESET}!{Fore.YELLOW}] {Fore.RESET}Error: " + line)
                 with open ("error.txt", "a") as f:
                     f.write(line + "\n")
-                    count += 1
-            os.system(f"title Tiktok Username Checker - Checked {count}/{len(users)} - Remaining: {len(users)-count}")
+                    error += 1
+            os.system(f"title Tiktok Username Checker - Checked {count}/{len(users)} - Free: {free} - Taken: {taken} - Ratelimited: {ratelimited} - Error: {error}")
 
 
 clear()
